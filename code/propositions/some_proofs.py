787aad4143771d6dea8ee4dad7255398639c4af0
maayanna <58308540+maayanna@users.noreply.github.com>
2021-10-28 15:44:12 +0300

    Add files via upload

---
+++ code/propositions/some_proofs.py
@@ -0,0 +1,294 @@
+# (c) This file is part of the course
+# Mathematical Logic through Programming
+# by Gonczarowski and Nisan.
+# File name: propositions/some_proofs.py
+
+"""Some proofs in propositional logic."""
+
+from propositions.syntax import *
+from propositions.proofs import *
+from propositions.axiomatic_systems import *
+from propositions.deduction import *
+
+# Some inference rules that only use conjunction.
+
+#: Conjunction introduction inference rule
+A_RULE = InferenceRule([Formula.parse('x'), Formula.parse('y')],
+                       Formula.parse('(x&y)'))
+#: Conjunction elimination (right) inference rule
+AE1_RULE = InferenceRule([Formula.parse('(x&y)')],Formula.parse('y'))
+#: Conjunction elimination (left) inference rule
+AE2_RULE = InferenceRule([Formula.parse('(x&y)')],Formula.parse('x'))
+
+def prove_and_commutativity() -> Proof:
+    """Proves ``'(q&p)'`` from ``'(p&q)'`` via `A_RULE`, `AE2_RULE`, and
+    `AE1_RULE`.
+
+    Returns:
+        A valid proof of ``'(q&p)'`` from the single assumption ``'(p&q)'`` via
+        the inference rules `A_RULE`, `AE2_RULE`, and `AE1_RULE`.
+    """
+    # Task 4.7
+
+
+    line_ea1 = Proof.Line(Formula.parse("q"), AE1_RULE, [0])
+    line_ea2 = Proof.Line(Formula.parse("p"), AE2_RULE, [0])
+    line_ea = Proof.Line(Formula.parse("(q&p)"), A_RULE, [1,2])
+
+    statement = InferenceRule([Formula.parse("(p&q)")], Formula.parse("(q&p)"))
+
+    rules ={ A_RULE, AE1_RULE, AE2_RULE }
+
+    return Proof(statement,rules, [Proof.Line(Formula.parse("(p&q)")), line_ea1, line_ea2, line_ea])
+
+
+def prove_I0() -> Proof:
+    """Proves `~propositions.axiomatic_systems.I0` via
+    `~propositions.axiomatic_systems.MP`, `~propositions.axiomatic_systems.I1`,
+    and `~propositions.axiomatic_systems.D`.
+
+    Returns:
+        A valid proof of `~propositions.axiomatic_systems.I0` from no
+        assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I1`, and
+        `~propositions.axiomatic_systems.D`.
+    """
+    # Task 4.8
+    line_D = Proof.Line(Formula.parse("((p->((p->p)->p))->((q->(q->p))->(p->p)))"), D, [])
+    line_I1 = Proof.Line(Formula.parse("(q->(p->q))"), I1, [])
+    line_MP = Proof.Line(Formula.parse("((p->(p->p))->(p->p))"), MP, [1,0])
+    line_from_I1 = Proof.Line(Formula.parse("(p->(p->p))"), I1, [])
+    line_from_MP = Proof.Line(Formula.parse("(p->p)"), MP, [3,2])
+
+    statement = InferenceRule([], Formula.parse("(p->p)"))
+
+    rules ={ I1, MP, D }
+
+    return Proof(statement,rules, [line_D, line_I1, line_MP, line_from_I1, line_from_MP])
+
+#: Hypothetical syllogism
+HS = InferenceRule([Formula.parse('(p->q)'), Formula.parse('(q->r)')],
+                   Formula.parse('(p->r)'))
+
+def prove_hypothetical_syllogism() -> Proof:
+    """Proves `HS` via `~propositions.axiomatic_systems.MP`,
+    `~propositions.axiomatic_systems.I0`, `~propositions.axiomatic_systems.I1`,
+    and `~propositions.axiomatic_systems.D`.
+
+    Returns:
+        A valid proof of `HS` from no assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`, and
+        `~propositions.axiomatic_systems.D`.
+    """
+    # Task 5.5
+
+    first_line = Proof.Line(Formula.parse("(p->q)"))
+    scd_line = Proof.Line(Formula.parse("(q->r)"))
+    third = Proof.Line(Formula.parse("((p->(q->r))->((p->q)->(p->r)))"), D, [])
+    fourth = Proof.Line(Formula.parse("((q->r)->(p->(q->r)))"), I1, [])
+    fifth = Proof.Line(Formula.parse("(p->(q->r))"), MP, [1, 3])
+    six = Proof.Line(Formula.parse("((p->q)->(p->r))"), MP, [4, 2])
+    seven_line = Proof.Line(Formula.parse("(p->r)"), MP, [0, 5])
+
+    my_lines = [first_line, scd_line, third, fourth, fifth, six, seven_line]
+    my_rules = {MP, I0, I1, D}
+
+    return Proof(HS, my_rules, my_lines)
+
+
+def prove_I2() -> Proof:
+    """Proves `~propositions.axiomatic_systems.I2` via
+    `~propositions.axiomatic_systems.MP`, `~propositions.axiomatic_systems.I0`,
+    `~propositions.axiomatic_systems.I1`, `~propositions.axiomatic_systems.D`,
+    and `~propositions.axiomatic_systems.N`.
+
+    Returns:
+        A valid proof of `~propositions.axiomatic_systems.I2` from no
+        assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.N`.
+    """
+    # Optional Task 6.7a
+
+#: Double-negation elimination
+NNE = InferenceRule([], Formula.parse('(~~p->p)'))
+
+def prove_NNE() -> Proof:
+    """Proves `NNE` via `~propositions.axiomatic_systems.MP`,
+    `~propositions.axiomatic_systems.I0`, `~propositions.axiomatic_systems.I1`,
+    `~propositions.axiomatic_systems.D`, and
+    `~propositions.axiomatic_systems.N`.
+
+    Returns:
+        A valid proof of `NNE` from no assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.N`.
+    """
+    # Optional Task 6.7b
+
+def prove_NN() -> Proof:
+    """Proves `~propositions.axiomatic_systems.NN` via
+    `~propositions.axiomatic_systems.MP`, `~propositions.axiomatic_systems.I0`,
+    `~propositions.axiomatic_systems.I1`, `~propositions.axiomatic_systems.D`,
+    and `~propositions.axiomatic_systems.N`.
+
+    Returns:
+        A valid proof of `~propositions.axiomatic_systems.NN` from no
+        assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.N`.
+    """
+    # Optional Task 6.7c
+
+#: Contraposition
+CP = InferenceRule([], Formula.parse('((p->q)->(~q->~p))'))
+
+def prove_CP() -> Proof:
+    """Proves `CP` via `~propositions.axiomatic_systems.MP`,
+    `~propositions.axiomatic_systems.I0`, `~propositions.axiomatic_systems.I1`,
+    `~propositions.axiomatic_systems.D`, and
+    `~propositions.axiomatic_systems.N`.
+
+    Returns:
+        A valid proof of `CP` from no assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.N`.
+    """
+    # Optional Task 6.7d
+
+def prove_NI() -> Proof:
+    """Proves `~propositions.axiomatic_systems.NI` via
+    `~propositions.axiomatic_systems.MP`, `~propositions.axiomatic_systems.I0`,
+    `~propositions.axiomatic_systems.I1`, `~propositions.axiomatic_systems.D`,
+    and `~propositions.axiomatic_systems.N`.
+
+    Returns:
+        A valid proof of `~propositions.axiomatic_systems.NI` from no
+        assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.N`.
+    """
+    # Optional Task 6.7e
+
+#: Consequentia mirabilis
+CM = InferenceRule([Formula.parse('(~p->p)')], Formula.parse('p'))
+
+def prove_CM() -> Proof:
+    """Proves `CM` via `~propositions.axiomatic_systems.MP`,
+    `~propositions.axiomatic_systems.I0`, `~propositions.axiomatic_systems.I1`,
+    `~propositions.axiomatic_systems.D`, and
+    `~propositions.axiomatic_systems.N`.
+
+    Returns:
+        A valid proof of `CM` from no assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.N`.
+    """
+    # Optional Task 6.7f
+
+def prove_R() -> Proof:
+    """Proves `~propositions.axiomatic_systems.R` via
+    `~propositions.axiomatic_systems.MP`, `~propositions.axiomatic_systems.I0`,
+    `~propositions.axiomatic_systems.I1`, `~propositions.axiomatic_systems.D`,
+    and `~propositions.axiomatic_systems.N`.
+
+    Returns:
+        A valid proof of `~propositions.axiomatic_systems.R` from no assumptions
+        via the inference rules `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.N`.
+    """
+    # Optional Task 6.7g
+
+def prove_N() -> Proof:
+    """Proves `~propositions.axiomatic_systems.N` via
+    `~propositions.axiomatic_systems.MP`, `~propositions.axiomatic_systems.I0`,
+    `~propositions.axiomatic_systems.I1`, `~propositions.axiomatic_systems.D`,
+    and `~propositions.axiomatic_systems.N_ALTERNATIVE`.
+
+    Returns:
+        A valid proof of `~propositions.axiomatic_systems.N` from no assumptions
+        via the inference rules `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.N_ALTERNATIVE`.
+    """
+    # Optional Task 6.8
+
+def prove_NA1() -> Proof:
+    """Proves `~propositions.axiomatic_systems.NA1` via
+    `~propositions.axiomatic_systems.MP`, `~propositions.axiomatic_systems.I0`,
+    `~propositions.axiomatic_systems.I1`, `~propositions.axiomatic_systems.D`,
+    `~propositions.axiomatic_systems.N`, and
+    `~propositions.axiomatic_systems.AE1`.
+
+    Returns:
+        A valid proof of `~propositions.axiomatic_systems.NA1` from no
+        assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.AE1`.
+    """
+    # Optional Task 6.9a
+
+def prove_NA2() -> Proof:
+    """Proves `~propositions.axiomatic_systems.NA2` via
+    `~propositions.axiomatic_systems.MP`, `~propositions.axiomatic_systems.I0`,
+    `~propositions.axiomatic_systems.I1`, `~propositions.axiomatic_systems.D`,
+    `~propositions.axiomatic_systems.N`, and
+    `~propositions.axiomatic_systems.AE2`.
+
+    Returns:
+        A valid proof of `~propositions.axiomatic_systems.NA2` from no
+        assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.AE2`.
+    """
+    # Optional Task 6.9b
+
+def prove_NO() -> Proof:
+    """Proves `~propositions.axiomatic_systems.NO` via
+    `~propositions.axiomatic_systems.MP`, `~propositions.axiomatic_systems.I0`,
+    `~propositions.axiomatic_systems.I1`, `~propositions.axiomatic_systems.D`,
+    `~propositions.axiomatic_systems.N`, and
+    `~propositions.axiomatic_systems.OE`.
+
+    Returns:
+        A valid proof of `~propositions.axiomatic_systems.NO` from no
+        assumptions via the inference rules
+        `~propositions.axiomatic_systems.MP`,
+        `~propositions.axiomatic_systems.I0`,
+        `~propositions.axiomatic_systems.I1`,
+        `~propositions.axiomatic_systems.D`, and
+        `~propositions.axiomatic_systems.OE`.
+    """
+    # Optional Task 6.9c
